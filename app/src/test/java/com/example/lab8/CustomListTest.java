79150394cad8ca2214ea6d4045e7bf1b62fef628
Shen Ruijun <ruijun2@ualberta.ca>
2023-03-07 18:04:20 -0700

    GREEN: passed test for delete city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -50,6 +50,22 @@ public class CustomListTest {
 
     }
 
+    @Test
+    public void testDeleteCity()
+    {
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+        list.deleteCity(city);
+        assertFalse(list.hasCity(city));
+        assertEquals(0, list.getCount());
+
+    }
+
+
+
+
 
 
 
